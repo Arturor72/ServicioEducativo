0e857b9460f7200377294a7fad0d4faaff45c031
Arturor72 <arturo_r72@hotmail.com>
2014-09-20 12:57:38 -0500

    StConnection and Usuario Dto

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/connection/StConnection.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/connection/StConnection.java
@@ -3,14 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.unfv.fiei.sistemat.model.connection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import pe.unfv.fiei.sistemat.constants.SistemTConstants;
 
 /**
  *
  * @author Arturo
  */
 public class StConnection {
-    
-    
+
+    public static Logger log4j = Logger.getLogger(StConnection.class);
+
+    public StConnection() {
+
+    }
+
+    public Connection getConnection() {
+        log4j.info("+ Init getConnection");
+        Connection cn = null;
+        try {
+            Class.forName(SistemTConstants.SISTEMAT_DRIVER).newInstance();
+            cn = DriverManager.getConnection(SistemTConstants.SISTEMAT_URL + SistemTConstants.SISTEMAT_BDNAME,
+                    SistemTConstants.SISTEMAT_USER, SistemTConstants.SISTEMAT_PASSWORD);
+            log4j.info("Connection succesful !!!");
+        } catch (ClassNotFoundException ex) {
+            log4j.error(ex.getMessage());
+        } catch (InstantiationException ex) {
+            log4j.error(ex.getMessage());
+        } catch (IllegalAccessException ex) {
+            log4j.error(ex.getMessage());
+        } catch (SQLException ex) {
+            log4j.error(ex.getMessage());
+        }
+        log4j.info("- Finish getConnection");
+        return cn;
+    }
+
 }
---
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dto/Usuario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.unfv.fiei.sistemat.model.dto;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Usuario {
+
+    private Integer Usr_Id;
+    private Integer Tip_Usr_Id;
+    private String Esp_Id;
+    private String Usr_Cod;
+    private String Usr_Nom;
+    private String Usr_Apat;
+    private String Usr_Amat;
+    private String Usr_Dni;
+    private Integer Usr_Gen;
+    private String Usr_Cel;
+    private String Usr_Mail;
+    private String Usr_User;
+    private String Usr_Pass;
+    private Integer Usr_Est;
+    
+    
+    
+
+}
