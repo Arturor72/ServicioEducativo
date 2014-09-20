f01e54b179a6c29f29db3c6427ad8b3fb4933e09
Arturor72 <arturo_r72@hotmail.com>
2014-09-20 13:16:00 -0500

    DaoUsuario as Interface

---
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/DaoUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.unfv.fiei.sistemat.model.dao;
+
+import pe.unfv.fiei.sistemat.model.dto.Usuario;
+
+/**
+ *
+ * @author Arturo
+ */
+public interface DaoUsuario {
+        public Usuario Login(String user, String password);
+}
