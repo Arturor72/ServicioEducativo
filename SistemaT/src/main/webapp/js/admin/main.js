3ee8efd3d8d5981682c43a1b5e70bb5e2565e23d
jccr0505 <jccr0505@gmail.com>
2014-09-23 23:59:53 -0500

    CRUD USUARIO

---
+++ SistemaT/src/main/webapp/js/admin/main.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function admin_QRY(path) {
+    window.location = path + "/UsuarioServlet?operation=QRY"
+}
+
+function curso_QRY(path) {
+    window.location = path + "/CursoServlet?operation=QRY"
+}
+
+
