f22c838e2850d9f5e89ed957da1ef8ae685524b0
jccr0505 <jccr0505@gmail.com>
2014-09-23 23:14:25 -0500

    CRUD USUARIO

--- SistemaT/src/main/webapp/js/admin/crud.js
+++
@@ -1,9 +0,0 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function admin_QRY(){
-    window.location = "UsuarioServlet?operation=QRY";
-}
-
