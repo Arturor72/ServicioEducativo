4e479d2b28c29ae665c4c9c119a18cd156c02de7
Arturor72 <arturo_r72@hotmail.com>
2014-09-24 21:55:03 -0500

    new CONSTANTS CURSO

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/constants/SistemTConstants.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/constants/SistemTConstants.java
@@ -16,6 +16,12 @@ public class SistemTConstants {
         /* LOGIN Constants */        
         public static final String LOGIN_SELECT = "SELECT usr_id, usr_cod, tip_usr_id, usr_nom, usr_apat, usr_amat, usr_dni, usr_gen, usr_cel, usr_mail, usr_user, AES_DECRYPT(usr_pass, 'F1O2R3114'), usr_est, esp_id FROM tbl_usuario WHERE (usr_user=?) and (AES_DECRYPT(usr_pass,'F1O2R3114') = ?)";
         public static final String USER_SELECT = "SELECT usr_id, usr_cod, tip_usr_id, usr_nom, usr_apat, usr_amat, usr_dni, usr_gen, usr_cel, usr_mail, usr_user, usr_pass, usr_est, esp_id FROM dbservicio.tbl_usuario WHERE tip_usr_id = ? AND esp_id = ?";
+        
+        
+        /* CURSO Constants */        
         public static final String CURSO_SELECT = "SELECT cur_id, cur_cod, cur_nom, esp_id, cur_est FROM tbl_curso WHERE esp_id = ?";    
+        public static final String CURSO_UPDATE = "UPDATE tbl_curso SET cur_cod=?, cur_nom=?, esp_id=?, cur_est=? WHERE cur_id = ?";    
+        public static final String CURSO_DELETE = "UPDATE tbl_curso SET cur_est=FALSE WHERE cur_id = ?";    
+        public static final String CURSO_GET = "SELECT cur_id, cur_cod, cur_nom, esp_id, cur_est FROM tbl_curso WHERE esp_id = ? AND cur_id=?";    
         
 }
