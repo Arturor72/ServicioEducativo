e03e0d8d3d100010fd0a653eef34952f506ba553
Arturor72 <arturo_r72@hotmail.com>
2014-09-24 21:55:14 -0500

    NEW METHODS

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/DaoCurso.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/DaoCurso.java
@@ -3,17 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.unfv.fiei.sistemat.model.dao;
 
 import java.util.List;
 import pe.unfv.fiei.sistemat.model.dto.Curso;
-
 
 /**
  *
  * @author Arturo
  */
 public interface DaoCurso {
+
     public List<Curso> cursoQry(Integer esp_id);
+
+    public Curso getCurso(Integer cur_id,Integer esp_id);
+    
+    public String cursoUpd(Curso curso);
+    
+    public String cursoDelete(List<Integer> lst);
+            
+
 }
