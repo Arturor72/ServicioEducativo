a75c5afc6fb9cee9f9d915a03e3aa097ef3948bf
Arturor72 <arturo_r72@hotmail.com>
2014-10-18 11:11:40 -0500

    Impl new methos for Servicio

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoServicioImpl.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoServicioImpl.java
@@ -144,6 +144,144 @@ public class DaoServicioImpl implements DaoServicio {
         return result;
     }
 
+    @Override
+    public Servicio ServicioGet(Integer serv_id) {
+        log4j.info("+init ServicioGet");
+        Servicio servicio = null;
+        String sql = SistemTConstants.SERVICIO_GET;
+        Connection cn = db.getConnection();
+        if (cn != null) {
+            try {
+                PreparedStatement psmt = cn.prepareStatement(sql);
+                psmt.setInt(1, serv_id);
+                ResultSet rs = psmt.executeQuery();
+                DaoAmbiente daoAmbiente = new DaoAmbienteImpl();
+                DaoSede daoSede = new DaoSedeImpl();
+                DaoTipoServicio daoTipoServicio = new DaoTipoServicioImpl();
+                if (rs.next()) {
+                    servicio = new Servicio();
+                    servicio.setSer_edu_id(rs.getInt(1));
+                    servicio.setSer_edu_fec(rs.getDate(2));
+                    servicio.setSer_edu_hin(String.valueOf(rs.getTime(3)));
+
+                    String csql = SistemTConstants.CURSO_GET_SERVICIO;
+                    psmt = cn.prepareStatement(csql);
+                    psmt.setInt(1, rs.getInt(4));
+                    ResultSet r2 = psmt.executeQuery();
+                    servicio.setCur_id(getCurso(r2));
+
+                    servicio.setAmb_id(daoAmbiente.getAmbiente(rs.getInt(5)));
+                    servicio.setSed_id(daoSede.getSede(rs.getInt(6)));
+                    servicio.setTip_serv_id(daoTipoServicio.getTipoServicio(rs.getInt(7)));
+
+                    csql = SistemTConstants.USER_GET;
+                    psmt = cn.prepareStatement(csql);
+                    psmt.setInt(1, rs.getInt(8));
+                    r2 = psmt.executeQuery();
+                    servicio.setUsr_adm_id(getUser(r2));
+                    csql = SistemTConstants.USER_GET;
+                    psmt = cn.prepareStatement(csql);
+                    psmt.setInt(1, rs.getInt(9));
+                    r2 = psmt.executeQuery();
+                    servicio.setUsr_tut_id(getUser(r2));
+                    servicio.setSer_edu_asist(rs.getInt(10));
+                    servicio.setSer_edu_desc(rs.getString(11));
+                    servicio.setSer_edu_est(rs.getInt(12));
+                }
+            } catch (SQLException e) {
+                log4j.error(e.getMessage());
+            } finally {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    log4j.error(e.getMessage());
+                }
+            }
+        }
+
+        log4j.info("+finish ServicioGet");
+        return servicio;
+    }
+
+    @Override
+    public String ServicioUpd(Servicio servicio) {
+        log4j.info("+init ServicioUpd");
+        String result = null;
+        String sql = SistemTConstants.SERVICIO_UPDATE;
+        Connection cn = db.getConnection();
+        if (cn != null) {
+            try {
+//                public static final String SERVICIO_UPDATE = "UDPATE dbservicio.tbl_servicio_educativo SET ser_edu_fec=?, ser_edu_hin=?, cur_id=?, amb_id=?, sed_id=?, tip_serv_id=?, usr_adm_id=?, usr_tut_id=?, ser_edu_asist=?, ser_edu_desc=?, ser_edu_est=?  WHERE ser_edu_id = ?";
+                PreparedStatement psmt = cn.prepareStatement(sql);
+                psmt.setDate(1, servicio.getSer_edu_fec());
+                psmt.setTime(2, Time.valueOf(servicio.getSer_edu_hin()));
+                psmt.setInt(3, servicio.getCur_id().getCur_id());
+                psmt.setInt(4, servicio.getAmb_id().getAmd_id());
+                psmt.setInt(5, servicio.getSed_id().getSed_id());
+                psmt.setInt(6, servicio.getTip_serv_id().getTip_ser_id());
+                psmt.setInt(7, servicio.getUsr_adm_id().getUsr_id());
+                psmt.setInt(8, servicio.getUsr_tut_id().getUsr_id());
+                psmt.setInt(9, servicio.getSer_edu_asist());
+                psmt.setString(10, servicio.getSer_edu_desc());
+                psmt.setInt(11, servicio.getSer_edu_est());
+                psmt.setInt(12, servicio.getSer_edu_id());
+                int r = psmt.executeUpdate();
+                if (r <= 0) {
+                    result = "No se actualizo";
+                    log4j.error(result);
+                }
+            } catch (SQLException e) {
+                log4j.error(e.getMessage());
+                result = "Error: " + e.getMessage();
+            } finally {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    log4j.error(e.getMessage());
+                    result = "Error: " + e.getMessage();
+                }
+            }
+
+        }
+        log4j.info("-finish ServicioUpd");
+        return result;
+    }
+
+    @Override
+    public String ServicioDel(List<Integer> lst) {
+        log4j.info("+init ServicioDel");
+        String sql = SistemTConstants.SERVICIO_DELETE;
+        Connection cn = db.getConnection();
+        String result = null;
+        if (cn != null) {
+            try {
+                PreparedStatement psmt = cn.prepareStatement(sql);
+                for (Integer x : lst) {
+                    psmt.setInt(1, x);
+                    int r = psmt.executeUpdate();
+                    if (r == 0) {
+                        result = "No se elimino" + x;
+                        log4j.error(result);
+                    }
+                }
+
+            } catch (SQLException e) {
+                log4j.error(e.getMessage());
+                result = "Error: " + e.getMessage();
+            } finally {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    log4j.error(e.getMessage());
+                    result = "Error: " + e.getMessage();
+
+                }
+            }
+        }
+        log4j.info("-finish ServicioDel");
+        return result;
+    }
+
     private Curso getCurso(ResultSet rs) throws SQLException {
         Curso curso = new Curso();
         if (rs.next()) {
@@ -174,8 +312,8 @@ public class DaoServicioImpl implements DaoServicio {
             u.setUsr_pass(rs.getString(12));
             u.setUsr_est(rs.getInt(13));
             u.setEsp_id(rs.getInt(14));
-
         }
         return u;
     }
+
 }
