c1af428209886b9123ad3fe3ef72870b23058d95
Arturor72 <arturo_r72@hotmail.com>
2014-10-19 09:38:10 -0500

    add validation for unique field

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoCursoImpl.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoCursoImpl.java
@@ -139,8 +139,7 @@ public class DaoCursoImpl implements DaoCurso {
         return result;
     }
 
-    
-     @Override
+    @Override
     public String cursoIns(Curso curso) {
         log4j.info("+init cursoIns");
         String sql = SistemTConstants.CURSO_INS;
@@ -160,14 +159,15 @@ public class DaoCursoImpl implements DaoCurso {
                 }
 
             } catch (SQLException e) {
-                log4j.error(e.getMessage());
-                result = "Error: " + e.getMessage();
+                log4j.error(e.getMessage() + " ERROR: " + e.getErrorCode());
+                result = String.valueOf(e.getErrorCode());
+
             } finally {
                 try {
                     cn.close();
                 } catch (SQLException e) {
                     log4j.error(e.getMessage());
-                    result = "Error: " + e.getMessage();
+                    result = String.valueOf(e.getErrorCode());
 
                 }
             }
@@ -175,7 +175,7 @@ public class DaoCursoImpl implements DaoCurso {
         log4j.info("-finish cursoIns");
         return result;
     }
-    
+
     @Override
     public String cursoDelete(List<Integer> lst) {
         log4j.info("+init cursoDelete");
