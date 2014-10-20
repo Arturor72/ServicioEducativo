89ece4dd8ff2db56a20634ada334759384590976
Arturor72 <arturo_r72@hotmail.com>
2014-10-19 22:48:53 -0500

    Implement new Method AMbiente

--- SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoAmbienteImpl.java
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dao/impl/DaoAmbienteImpl.java
@@ -5,10 +5,13 @@
  */
 package pe.unfv.fiei.sistemat.model.dao.impl;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.log4j.Logger;
 import pe.unfv.fiei.sistemat.constants.SistemTConstants;
 import pe.unfv.fiei.sistemat.model.connection.StConnection;
@@ -20,14 +23,14 @@ import pe.unfv.fiei.sistemat.model.dto.Ambiente;
  * @author Arturo
  */
 public class DaoAmbienteImpl implements DaoAmbiente {
-    
+
     Logger log4j = Logger.getLogger(DaoAmbienteImpl.class);
     StConnection db;
-    
+
     public DaoAmbienteImpl() {
         db = new StConnection();
     }
-    
+
     @Override
     public Ambiente getAmbiente(Integer amb_id) {
         log4j.info("+init getAmbiente");
@@ -56,9 +59,51 @@ public class DaoAmbienteImpl implements DaoAmbiente {
                 }
             }
         }
-        
+
         log4j.info("-finish getAmbiente");
         return ambiente;
     }
-    
+
+    @Override
+    public List<Ambiente> dispAmbienteQry(String fecha, String hora, Integer serv_id, Integer sed_id) {
+        log4j.info("+init dispAmbienteQry");
+        List<Ambiente> list = null;
+        Connection cn = db.getConnection();
+        String sql = SistemTConstants.AMBIENTE_GET_LIST_DISPONIBLE;
+        if (cn != null) {
+            try {
+                CallableStatement cs = cn.prepareCall(sql);
+                cs.setString(1, fecha);
+                cs.setString(2, hora);
+                cs.setInt(3, serv_id);
+                cs.setInt(4, sed_id);
+                ResultSet rs = cs.executeQuery();
+                list = new LinkedList<Ambiente>();
+                while (rs.next()) {
+                    if (rs.getTime(4) == null) {
+                        Ambiente ambiente = new Ambiente();
+                        ambiente.setAmd_id(rs.getInt(1));
+                        ambiente.setSed_id(sed_id);
+                        ambiente.setAmb_den(rs.getString(2));
+                        list.add(ambiente);
+                    }
+
+                }
+            } catch (SQLException e) {
+                log4j.error(e.getMessage());
+            } finally {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    log4j.error(e.getMessage());
+                }
+            }
+
+        }
+
+        log4j.info("-finish dispAmbienteQry");
+        return list;
+
+    }
+
 }
