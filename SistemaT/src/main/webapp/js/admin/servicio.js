39274a6909c6fd7be95003f45e423c6a2594f94a
fernt14 <fernt14@hotmail.com>
2014-10-23 00:11:51 -0500

    servcio cambio

--- SistemaT/src/main/webapp/js/admin/servicio.js
+++ SistemaT/src/main/webapp/js/admin/servicio.js
@@ -166,14 +166,14 @@ function agregarServicios(Datos) {
 $(function () {
     $('#datetimepicker1').datetimepicker({
         pick12HourFormat: false,
-        minuteStepping: 15,
+        minuteStepping: 1,
         language: 'es'
     });
 
 
     $('#fecha_hora_ins').datetimepicker({
         pick12HourFormat: false,
-        minuteStepping: 15,
+        minuteStepping: 1,
         language: 'es'
     });
 
@@ -228,25 +228,25 @@ function actualizarServicio() {
 
 
 
-function crearFormServicio() {
+function guardarFormServicio() {
 
 
     // var cur_id = $('#curso_ins:disabled');
     $('#servicio_ins').prop('disabled', true);
     $('#servicio_ins').val(0);
-    
+
     $('#fecha_hora_ins').data("DateTimePicker").disable();
     $('#fecha_hora_ins').data("DateTimePicker").setDate(new Date());
-    
+
     $('#sede_ins').prop('disabled', true);
     $('#sede_ins').val(0);
-    
+
     $('#ambiente_ins').prop('disabled', true);
     $('#ambiente_ins').val(0);
-    
+
     $('#tutor_ins').prop('disabled', true);
     $('#tutor_ins').val(0);
-    
+
     $('#myModalIns').modal('show');
 
 
@@ -271,9 +271,9 @@ function selectCurso() {
 
         $('#ambiente_ins').prop('disabled', true);
         $('#ambiente_ins').val(0);
-        
-            $('#tutor_ins').prop('disabled', true);
-         $('#tutor_ins').val(0);
+
+        $('#tutor_ins').prop('disabled', true);
+        $('#tutor_ins').val(0);
     }
 }
 
@@ -292,9 +292,9 @@ function selectServicio() {
 
         $('#ambiente_ins').prop('disabled', true);
         $('#ambiente_ins').val(0);
-         
+
         $('#tutor_ins').prop('disabled', true);
-         $('#tutor_ins').val(0);
+        $('#tutor_ins').val(0);
     }
 }
 
@@ -340,7 +340,7 @@ function selectSede() {
 
             },
             success: function (response) {
-             //alert(response);
+                //alert(response);
                 console.log(response);
 
                 var Datos = JSON.parse(response);
@@ -367,42 +367,42 @@ function selectSede() {
 
         ambiente_ins.prop('disabled', true);
         ambiente_ins.val(0);
-        
+
         $('#tutor_ins').prop('disabled', true);
         $('#tutor_ins').val(0);
     }
 }
 
-function selectAmbiente(){
-    
-var operation = "QRY_DISP";
-  
+function selectAmbiente() {
+
+    var operation = "QRY_DISP";
+
     var tip_serv_id = $('#servicio_ins').val();
 
     var total_fecha = $('#fecha_hora_ins').data("DateTimePicker").getDate();
     var fecha = moment(total_fecha).format('YYYY-MM-DD');
     var hora = moment(total_fecha).format('HH:mm:ss');
     var sed_id = $('#sede_ins').val();
-     var amb_id = $('#ambiente_ins').val();
-    var tutor_ins= $('#tutor_ins');
-     var tip_user_id=2;
+    var amb_id = $('#ambiente_ins').val();
+    var tutor_ins = $('#tutor_ins');
+    var tip_user_id = 2;
 
 
-    console.log(fecha + " " + hora + " " +  " tipo serv: "+tip_serv_id + " aamb " +amb_id);
+    // console.log(fecha + " " + hora + " " + " tipo serv: " + tip_serv_id + " aamb " + amb_id);
 
 
     var parametros = {
         operation: operation,
         fecha: fecha,
         hora: hora,
-       tip_serv_id: tip_serv_id,
-        tip_user_id:tip_user_id
+        tip_serv_id: tip_serv_id,
+        tip_user_id: tip_user_id
 
 
     };
 
-    if (fecha.length > 0 && hora.length > 0 && tip_serv_id >  0 && amb_id > 0) {
-tutor_ins.prop('disabled', false);
+    if (fecha.length > 0 && hora.length > 0 && tip_serv_id > 0 && amb_id > 0) {
+        tutor_ins.prop('disabled', false);
         $.ajax({
             data: parametros,
             url: '/SistemaT/UsuarioServlet',
@@ -411,15 +411,15 @@ tutor_ins.prop('disabled', false);
 
             },
             success: function (response) {
-               alert(response);
-                console.log(response);
+                // alert(response);
+                //console.log(response);
 
                 var Datos = JSON.parse(response);
                 tutor_ins.empty();
                 tutor_ins.append('<option value="0">Seleccione</option>');
                 tutor_ins.prop('disabled', false);
                 for (i in Datos) {
-                    tutor_ins.append('<option value="' + Datos[i].usr_id + '">' + Datos[i].usr_nom +' '+ Datos[i].usr_apat +' '+ Datos[i].usr_amat + '</option>');
+                    tutor_ins.append('<option value="' + Datos[i].usr_id + '">' + Datos[i].usr_nom + ' ' + Datos[i].usr_apat + ' ' + Datos[i].usr_amat + '</option>');
                     //curso_ins.append('<option value="' + Datos[i].cur_id + '">' + Datos[i].cur_nom + '</option>');
 
                 }
@@ -433,7 +433,7 @@ tutor_ins.prop('disabled', false);
 
     }
     if (amb_id <= 0) {
-       tutor_ins.empty();
+        tutor_ins.empty();
         tutor_ins.append('<option value="0">Seleccione</option>');
 
         tutor_ins.prop('disabled', true);
@@ -441,4 +441,80 @@ tutor_ins.prop('disabled', false);
     }
 }
 
+
+function guardarServicio() {
+
+    var operation = "INS";
+
+    var tipo_serv_id = $('#servicio_ins').val();
+
+    var total_fecha = $('#fecha_hora_ins').data("DateTimePicker").getDate();
+    var ser_edu_fec = moment(total_fecha).format('YYYY-MM-DD');
+    var ser_edu_hin = moment(total_fecha).format('HH:mm:ss');
+
+    var sed_id = $('#sede_ins').val();
+    var amb_id = $('#ambiente_ins').val();
+    var usr_tut_id = $('#tutor_ins').val();
+    var curso_id = $('#curso_ins').val();
+
+    var ser_edu_desc = $('#descripcion_ins').val();
+
+
+    //console.log(tip_serv_id );
+
+    var parametros = {
+        operation: operation,
+        ser_edu_fec: ser_edu_fec,
+        ser_edu_hin: ser_edu_hin,
+        curso_id: curso_id,
+        amb_id: amb_id,
+        sed_id: sed_id,
+        tipo_serv_id: tipo_serv_id,
+        usr_tut_id: usr_tut_id,
+        ser_edu_desc: ser_edu_desc
+
+
+    };
+
+    if (ser_edu_fec.length > 0 &&
+            ser_edu_hin.length > 0 &&
+            tipo_serv_id > 0 &&
+            curso_id > 0 &&
+            amb_id > 0 &&
+            sed_id > 0 &&
+            usr_tut_id > 0
+            ) {
+        $.ajax({
+            data: parametros,
+            url: '/SistemaT/ServicioServlet',
+            type: 'post',
+            beforeSend: function () {
+
+            },
+            success: function (response) {
+
+                if (response === 'error') {
+
+                    alert("no registro" + response);
+                }
+                else {
+                    alert("si registro" + response);
+
+                    $('#mensaje').addClass('alert alert-success');
+                    $('#mensaje').html('Curso creado satisfactoriamente');
+                    setTimeout(function () {
+                        url = "/SistemaT/admin/indexA.jsp";
+                        $(location).attr('href', url);
+                    }, 2000);
+                }
+
+
+
+            },
+            error: function (result, f) {
+                alert('ERROR ' + result.status + ' ' + result.statusText + ' ' + f);
+            }
+        });
+    }
+}
 
