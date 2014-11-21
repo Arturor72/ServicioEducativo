2f3201decbaa9a8257decf42971bc50bda3a5e37
Arturor72 <arturo_r72@hotmail.com>
2014-11-20 22:41:29 -0500

    AlumnoDTO

---
+++ SistemaT/src/main/java/pe/unfv/fiei/sistemat/model/dto/Alumno.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.unfv.fiei.sistemat.model.dto;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Alumno {
+    private Integer al_id;
+    private String al_cod;
+    private String al_nom;
+    private String al_apat;
+    private String al_amat;
+    private String al_cel;
+    private String al_mail;
+    private Integer al_susp;
+
+    public Integer getAl_id() {
+        return al_id;
+    }
+
+    public void setAl_id(Integer al_id) {
+        this.al_id = al_id;
+    }
+
+    public String getAl_cod() {
+        return al_cod;
+    }
+
+    public void setAl_cod(String al_cod) {
+        this.al_cod = al_cod;
+    }
+
+    public String getAl_nom() {
+        return al_nom;
+    }
+
+    public void setAl_nom(String al_nom) {
+        this.al_nom = al_nom;
+    }
+
+    public String getAl_apat() {
+        return al_apat;
+    }
+
+    public void setAl_apat(String al_apat) {
+        this.al_apat = al_apat;
+    }
+
+    public String getAl_amat() {
+        return al_amat;
+    }
+
+    public void setAl_amat(String al_amat) {
+        this.al_amat = al_amat;
+    }
+
+    public String getAl_cel() {
+        return al_cel;
+    }
+
+    public void setAl_cel(String al_cel) {
+        this.al_cel = al_cel;
+    }
+
+    public String getAl_mail() {
+        return al_mail;
+    }
+
+    public void setAl_mail(String al_mail) {
+        this.al_mail = al_mail;
+    }
+
+    public Integer getAl_susp() {
+        return al_susp;
+    }
+
+    public void setAl_susp(Integer al_susp) {
+        this.al_susp = al_susp;
+    }
+    
+    
+}
