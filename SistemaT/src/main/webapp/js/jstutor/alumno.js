fcd5a3d2e58538b738628019118f68d7d39fb731
Mariadicar6 <mariadicar.28@gmail.com>
2014-11-26 21:21:52 -0500

    alumno.js

--- SistemaT/src/main/webapp/js/jstutor/alumno.js
+++ SistemaT/src/main/webapp/js/jstutor/alumno.js
@@ -67,8 +67,100 @@ function agregarAlumnos(Datos, tabla) {
             Datos[i].al_cod,
             Datos[i].al_nom,
             Datos[i].al_apat + ' ' + Datos[i].al_amat,
-            '<input class="center-block" name="INS" type="checkbox" value="' + Datos[i].al_id + '">'
+             '<input class="center-check" name="SUS" type="checkbox" value="' + Datos[i].al_id + '"/>'
         ]);
     }
 
+}
+
+
+function suspenderAlumnos(){
+    
+    
+     var ids = [];
+
+    //var serEduId = $('#ser-id').val();
+     $("input[name='SUS']:checked").each(function() {
+        ids.push($(this).val());
+        alert("asd");
+    });
+    //console.log(serEduId);
+
+
+    var alId = ids.toString();
+    //alert(ids.toString());
+    var operation = 'SUSP_ALUMNOS';
+    // var serEduId=;
+    var parametros = {
+        operation: operation,
+        al_id: alId
+
+    };
+    console.log("holaaa");
+    
+    //alert("jj");
+    //alert(alId);
+    if (alId.length > 0){
+        $.ajax({
+            data: parametros,
+            url: '/SistemaT/AlumnoServlet',
+            type: 'post',
+            beforeSend: function () {
+                //var path = window.location.host;
+                // $("#servicio").html('<img  class="center-block" src="http://'+path+'//SistemaT/img/load.GIF"/>');
+            },
+            success: function (response) {
+                
+                alert("dentro del response "+response);
+                if (response === 'error') {
+
+                    $('#mensaje_ins').empty();
+                    $('#mensaje_ins').addClass('alert alert-danger');
+                    $('#mensaje_ins').html('No se pudo completar el registro');
+                }
+                else {
+                    // alert("si registro" + response);
+                    $('#mensaje_ins').empty();
+                    $('#mensaje_ins').removeClass('alert alert-danger');
+                    $('#mensaje_ins').addClass('alert alert-success');
+                    $('#mensaje_ins').html(response);
+                    setTimeout(function () {
+                        url = "/SistemaT/p_tutor/ralumnos/AlumnoQry.jsp";
+                        $(location).attr('href', url);
+                    }, 2000);
+                }
+            },
+            error: function (result, f) {
+                alert('ERROR ' + result.status + ' ' + result.statusText + ' ' + f);
+            }
+        });
+
+    }
+    else {
+        $('#mensaje_ins').empty();
+        $('#mensaje_ins').addClass('alert alert-danger');
+        $('#mensaje_ins').html('No se ha seleccionado ningun alumno');
+
+    }
+    
+}
+
+
+function mostrarMensajeSuspender() {
+    var ids = [];
+    $('#myModalMensajeDel').html('Suspender');
+    $('#myModalMensajeUpd').html('');
+
+    $("input[name='SUS']:checked").each(function() {
+   
+        ids.push($(this).val());
+    });
+    if (ids.length === 0) {
+        $('#modal-mensaje').html('Seleccione alumno(s) a suspender');
+        $('#myModalMensaje').modal('show');
+
+    } else {
+        $('#modal-mensaje-del').html('¿Estas seguro que quieres suspender?');
+        $('#myModalSus').modal('show');
+    }
 }
